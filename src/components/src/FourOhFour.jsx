8163823266207a23846920c9f4dcfe7a7dc9333f
Zane <zchristenson@tesla.com>
2017-07-31 22:10:43 -0700

    Fixed routing and added 404 page.

---
+++ src/components/src/FourOhFour.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './FourOhFour.scss';
+
+const FourOhFour = () => (
+  <div className={styles.container}>
+    404
+  </div>
+);
+
+export default FourOhFour;
